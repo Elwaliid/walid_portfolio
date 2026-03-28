63f57426063f4946ff1d04162ca879eaefaaed5e
walid <168366600+Elwaliid@users.noreply.github.com>
2026-03-27 20:17:15 -0700

    Update sections_view.dart

--- lib/sections/sections_view.dart
+++ lib/sections/sections_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mysite/sections/home/home.dart';
+import 'package:mysite/sections/services/services.dart';
 
 class Body {
   static List<Widget> views = [
     const HomePage(),
-    // const Services(),
+    const Services(),
     // const Portfolio(),
     // const Contact(),
     // const Footer(),

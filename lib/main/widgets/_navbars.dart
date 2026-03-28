e41f0c944e15920e4d7c7ab6e0aa83a593979cb1
walid <168366600+Elwaliid@users.noreply.github.com>
2026-03-27 17:24:59 -0700

    navbarsss for both mobile and desktop

--- lib/main/widgets/_navbars.dart
+++ lib/main/widgets/_navbars.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mysite/core/app_theme.dart';
+import 'package:mysite/core/providers/drawer_provider.dart';
+import 'package:mysite/core/responsivness/responsive.dart';
 import 'package:mysite/core/text_space_config/space.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 class _NavbarDesktop extends StatefulWidget {
   const _NavbarDesktop({Key? key}) : super(key: key);
@@ -28,6 +32,35 @@ class _NavbarDesktopState extends State<_NavbarDesktop> {
           //         index: e.key,
           //       ),
           //     ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NavBarTablet extends StatelessWidget {
+  const _NavBarTablet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final drawerProvider = Provider.of<DrawerProvider>(context);
+    var theme = Theme.of(context);
+    return Container(
+      color: theme.navBarColor,
+      padding: EdgeInsets.symmetric(
+          horizontal: Responsive.isTablet(context) ? 10.w : 10, vertical: 10),
+      child: Row(
+        children: [
+          IconButton(
+            highlightColor: Colors.white54,
+            onPressed: () {
+              drawerProvider.key.currentState!.openDrawer();
+            },
+            icon: const Icon(Icons.menu),
+          ),
+          Space.xm!,
+          // const NavBarLogo(),
+          // Space.x1!,
         ],
       ),
     );

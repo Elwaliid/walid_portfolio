b92ca0e6f5a216dc0abeb641c4a4bdfcb8bd47d9
walid <168366600+Elwaliid@users.noreply.github.com>
2026-03-28 18:41:51 -0700

    Create portfolio_desktop.dart

---
+++ lib/sections/portfolio/portfolio_desktop.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:mysite/changes/links.dart';
+import 'package:mysite/changes/strings.dart';
+import 'package:mysite/core/text_space_config/configs.dart';
+import 'package:mysite/core/url_launch.dart';
+import 'package:mysite/sections/portfolio/util/project_utils.dart';
+import 'package:mysite/sections/widgets/custom_text_heading.dart';
+
+import 'package:sizer/sizer.dart';
+
+import 'widgets/project_card.dart';
+
+class PortfolioDesktop extends StatefulWidget {
+  const PortfolioDesktop({Key? key}) : super(key: key);
+
+  @override
+  State<PortfolioDesktop> createState() => _PortfolioDesktopState();
+}
+
+class _PortfolioDesktopState extends State<PortfolioDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding:
+          EdgeInsets.symmetric(horizontal: size.width / 8).copyWith(top: 0),
+      child: Column(
+        children: [
+          const CustomSectionHeading(text: "\nProjects"),
+          Space.y(1.w)!,
+          CustomSectionSubHeading(text: protfolioSubHeading),
+          Space.y(1.w)!,
+          Wrap(
+            alignment: WrapAlignment.start,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            runSpacing: 3.w,
+            children: projectUtils
+                .asMap()
+                .entries
+                .map(
+                  (e) => ProjectCard(project: e.value),
+                )
+                .toList(),
+          ),
+          Space.y(1.5.w)!,
+          OutlinedButton(
+            onPressed: () => openURL(previews),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'View Previews',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
